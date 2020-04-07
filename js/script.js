433a78c26a4541bc818eda405f48fe887e7c7550
Karol <karolsobolewski92@gmail.com>
2020-04-07 21:31:23 +0200

    Fix code by definig functions as constants

--- js/script.js
+++ js/script.js
@@ -1,73 +1,75 @@
-let playerScore = 0;
-let computerScore = 0;
-function playGame(playerInput) {
+{
+  let playerScore = 0,
+    computerScore = 0;
 
-  function getMoveName(argMoveId){
-    if(argMoveId == 1) return 'kamień';
-    else if(argMoveId == 2) return 'papier';
-    else if(argMoveId == 3) return 'nożyce';
-  }
+  const playGame = function (playerInput) {
 
-  function displayResult(argComputerMove, argPlayerMove) {
-
-    printMessage('Zagrałem ' + argComputerMove + ', a Ty ' + argPlayerMove);
-    if((argComputerMove === 'kamień' && argPlayerMove === 'papier') ||
-       (argComputerMove === 'nożyce' && argPlayerMove == 'kamień') ||
-       (argComputerMove === 'papier' && argPlayerMove === 'nożyce')) {
-       playerScore = playerScore+1;
-      return 'Ty wygrywasz!!';
-
+    const getMoveName = function(argMoveId) {
+      if (argMoveId == 1) return 'kamień';
+      else if (argMoveId == 2) return 'papier';
+      else if (argMoveId == 3) return 'nożyce';
     }
-    else if(argComputerMove == argPlayerMove) return 'Remis!';
-    else{
-      computerScore = computerScore+1;
-      return 'Wygrał komputer!';
+
+    //console.log(getMoveName(playerInput));
+
+    const displayResult = function(argComputerMove, argPlayerMove) {
+      printMessage('Zagrałem ' + argComputerMove + ', a Ty ' + argPlayerMove);
+      if ((argComputerMove === 'kamień' && argPlayerMove === 'papier') ||
+      (argComputerMove === 'nożyce' && argPlayerMove === 'kamień') ||
+      (argComputerMove === 'papier' && argPlayerMove === 'nożyce')) {
+        playerScore = playerScore + 1;
+        return 'Ty wygrywasz!!';
+      } else if (argComputerMove == argPlayerMove) return 'Remis!';
+      else {
+        computerScore = computerScore + 1;
+        return 'Wygrał komputer!';
+      }
     }
-  }
 
   /* Proces gry */
 
-
   clearMessages();
-
 
   //let randomNumber = Math.floor(Math.random() * 3 + 1);
 
   let randomNumber = Math.random()
-    //console.log(Math.random());
-    if(Math.random() < 0.12)  {
-      //console.log('<12');
-    randomNumber = 1
-
+  //console.log(Math.random());
+    if (Math.random() < 0.12) {
+    //console.log('<12');
+      randomNumber = 1
   } else if (Math.random() <= 0.24) {
-      //console.log('<24');
-    randomNumber = 2
-    } else {
-      randomNumber = 3
-      //console.log('>24');
+    //console.log('<24');
+      randomNumber = 2
+  } else {
+    randomNumber = 3
+    //console.log('>24');
     }
 
-  let computerMove = getMoveName(randomNumber);
-  //console.log(computerMove);
+    const computerMove = getMoveName(randomNumber),
+    playerMove = getMoveName(playerInput);
+    printMessage(displayResult(computerMove, playerMove));
+    //inny kolor wyniku np. document.getElementById('scorebox').innerHTML = displayResult(computerMove, playerMove);
+    document.getElementById('player').innerHTML = playerScore;
+    document.getElementById('computer').innerHTML = computerScore;
+    //console.log(displayResult(computerMove, playerMove));
+    //console.log(computerMove);
+  }
 
-  let playerMove =  getMoveName(playerInput);
+  // for(let i = 0; i < 250; i++ ){
+  //   playGame(1)
+  // }
 
-  printMessage(displayResult(computerMove, playerMove));
-//inny kolor wyniku np. document.getElementById('scorebox').innerHTML = displayResult(computerMove, playerMove);
-  document.getElementById('player').innerHTML = playerScore;
-  document.getElementById('computer').innerHTML = computerScore;
-}
-
-function reset() {
+  const reset = function() {
   document.getElementById('messages').innerHTML = 'Kliknij przycisk';
-  playerScore = 0;
+  playerScore = 0; //,?
   computerScore = 0;
   document.getElementById('player').innerHTML = playerScore;
   document.getElementById('computer').innerHTML = computerScore;
 
-}
+  }
 
-document.getElementById('reset-btn').addEventListener('click', function(){ reset(); })
-document.getElementById('play-rock').addEventListener('click', function(){ playGame(1); });
-document.getElementById('play-paper').addEventListener('click', function(){ playGame(2); });
-document.getElementById('play-scissors').addEventListener('click', function(){ playGame(3); });
+  document.getElementById('reset-btn').addEventListener('click', function() { reset(); });
+  document.getElementById('play-rock').addEventListener('click', function() { playGame(1); });
+  document.getElementById('play-paper').addEventListener('click', function() { playGame(2); });
+  document.getElementById('play-scissors').addEventListener('click', function() { playGame(3); });
+}
